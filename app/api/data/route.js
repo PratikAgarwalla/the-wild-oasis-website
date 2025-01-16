b1b1670dabfcfa0ecb7ab72f757c6ce81de96c17
Pratik Agarwalla <pratikagarwalla2003@gmail.com>
2025-01-16 12:04:51 +0530

    Updated ' with &apos;

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -1,5 +1,5 @@
 export async function GET() {
-  return new Response(JSON.stringify({ name: "Pratik" }), {
+  return new Response(JSON.stringify({ name: "TEST" }), {
     headers: { "Content-Type": "application/json" },
   });
 }
